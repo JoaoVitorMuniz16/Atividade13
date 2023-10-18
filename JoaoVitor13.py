793cadad768ff4a3611fb9c4d912d2e274334695
João Vitor Muniz Vasconcelos <140927075+JoaoVitorMuniz16@users.noreply.github.com>
2023-10-18 11:14:13 -0300

    Add files via upload

---
+++ JoaoVitor13.py
@@ -0,0 +1,10 @@
+n1 = int(input("insira o primeiro número"))
+n2 = int(input("insira o segundo número"))
+n3 = int(input("insira o terceiro número"))
+
+if n1 > n2:
+    print (n1)
+elif n2 > n1:
+    print (n2)
+else:
+    print (n3)
